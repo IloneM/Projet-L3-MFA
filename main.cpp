ab6e8c317144de21fed5bbcf85326234fa50cf93
Ilone <ilonemail2@gmail.com>
2015-03-23 19:12:20 +0100

    clean dir struct

--- main.cpp
+++ main.cpp
@@ -1,27 +1,16 @@
-#include <libcmaes/cmaes.h>
+#include "rp_cma.hpp"
 #include <iostream>
 
-using namespace libcmaes;
+double testFunc(Eigen::RowVectorXd v) {
+	return v.norm();
+}
 
-FitFunc fsphere = [](const double *x, const int N)
-{
-  double val = 0.0;
-  for (int i=0;i<N;i++)
-    val += x[i]*x[i];
-  return val;
-};
+int main(int argc, char* argv[]) {
 
-int main(int argc, char *argv[])
-{
-  int dim = 10; // problem dimensions.
-  std::vector<double> x0(dim,10.0);
-  double sigma = 0.1;
-  int lambda = 100; // offsprings at each generation.
-  CMAParameters<> cmaparams(dim,&x0.front(),sigma);
-  cmaparams.set_algo(BIPOP_CMAES);
-  CMASolutions cmasols = cmaes<>(fsphere,cmaparams);
-  std::cout << "best solution: " << cmasols << std::endl;
-  std::cout << "optimization took " << cmasols.elapsed_time() / 1000.0 << " seconds\n";
-  return cmasols.run_status();
+	RP_CMA instance(10,2, testFunc);
+
+	instance.run();
+
+	return 0;
 }
 
