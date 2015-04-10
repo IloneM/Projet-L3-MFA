a1d454c3714e30a6ff1e884c1b0dbb40fec2c12c
Ilone <ilonemail2@gmail.com>
2015-04-10 22:01:44 +0200

    Setup main structure

--- rp_cma.hpp
+++ rp_cma.hpp
@@ -1,36 +1,16 @@
 #ifndef RP_CMA_H
 #define RP_CMA_H
 
-/*
- * Importants points procedures seems to be eval for N_D and ask for n_d.
- * A overall class must be implemented
- */
-
 #include <Eigen/Eigen>
 #include <eigenmvn.h>
 #include <cmaes.h>
 
+/*
+ * DISCLAMER: This class may be useless and is implemented only in case of potential uses in the future
+ */
+
 using namespace libcmaes;
 
-class RP_CMA : public CMAStrategy<CovarianceUpdate>
-{
-
-//general space properties
-
-protected:
-
-unsigned int m_D;//size of the big space
-unsigned int m_d;//size of the "small space" (i.e. d<<D)
-unsigned int m_K;//number of samples
-
-public:
-
-RP_CMA(FitFunc &func, CMAParameters<> &parameters, unsigned int D, unsigned int d) : CMAStrategy<CovarianceUpdate>(func,parameters),
-		m_d(d), m_D(D), m_K(10)/*to set*/
-{
-	
-}
-
-};
+class RP_CMA : public CMAStrategy<CovarianceUpdate> {};
 
 #endif
---
+++ rp_cma_small.cpp
@@ -0,0 +1,7 @@
+#include "rp_cma.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+
